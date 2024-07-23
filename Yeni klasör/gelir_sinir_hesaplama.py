1dfcd85f0595775641cdf1d2a225a361f0293a80
Engincndtr <engin.dutar.ecd@gmail.com>
2024-07-24 02:27:47 +0300

    gelir_sinir_hesaplama
    Istediğimiz bir geliri belirledigimiz bir sinirdan yuksek ya da kucuk oldugunu veren kod. Python ile gelistirildi.

---
+++ Yeni klasör/gelir_sinir_hesaplama.py
@@ -0,0 +1,9 @@
+b=a = int(input("sinir degeri giriniz:"))
+a = int(input("sayi giriniz:"))
+
+if (a > b):
+    print("gelir sinirdan yuksek")
+elif (a < b):
+    print("gelir sinirdan kucuk")
+else:
+    print("gelir sinira esit")
